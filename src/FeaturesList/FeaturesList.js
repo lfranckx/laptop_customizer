bad7fc526d7d372e6a99bbbba7fdab1b523c8d43
Bowie <bowie@thinkful.com>
2019-11-30 18:03:40 -0800

    update

--- src/FeaturesList/FeaturesList.js
+++ src/FeaturesList/FeaturesList.js
@@ -4,7 +4,7 @@ import Feature from '../Feature/Feature'
 class FeaturesList extends Component {
     render() {
         // console.log(this.props.features)
-        const features = Object.keys(this.props.features).map((feature, idx) => {
+        const featuresList = Object.keys(this.props.features).map((feature, idx) => {
             const featureHash = feature + '-' + idx;
             return (
                 <fieldset className="feature" key={featureHash}>
@@ -21,7 +21,7 @@ class FeaturesList extends Component {
 
         return (
             <div className="features-list">
-                {features}
+                {featuresList}
             </div>
         )
     }
