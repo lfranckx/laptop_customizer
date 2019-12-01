3cb65134d32336eac6578f14d1a1e69a4043aec8
Bowie <bowie@thinkful.com>
2019-11-30 16:49:48 -0800

    update

--- src/Feature/Feature.js
+++ src/Feature/Feature.js
@@ -7,23 +7,25 @@ class Feature extends Component {
     render() {
         console.log(this.props.features)
         const options = this.props.features.map(item => {
-            const itemHash = slugify(JSON.stringify(item));
-            return (
-              <div key={itemHash} className="feature__item">
-                <input
-                  type="radio"
-                  id={itemHash}
-                  className="feature__option"
-                  name={slugify(item)}
-                  checked={item.name === this.state.selected[item].name}
-                  onChange={e => this.updateFeature(item)}
-                />
-                <label htmlFor={itemHash} className="feature__label">
-                  {item.name} ({this.props.value.format(item.cost)})
-                </label>
-              </div>
-            );
-        })
+            const options = this.props.features[feature].map(item => {
+                const itemHash = slugify(JSON.stringify(item));
+                return (
+                  <div key={itemHash} className="feature__item">
+                    <input
+                      type="radio"
+                      id={itemHash}
+                      className="feature__option"
+                      name={slugify(feature)}
+                      checked={item.name === this.state.selected[feature].name}
+                      onChange={e => this.updateFeature(feature, item)}
+                    />
+                    <label htmlFor={itemHash} className="feature__label">
+                      {item.name} ({USCurrencyFormat.format(item.cost)})
+                    </label>
+                  </div>
+                );
+            });
+        });
         return (
             {options}
         )
--- src/FeaturesList/FeaturesList.js
+++ src/FeaturesList/FeaturesList.js
@@ -3,7 +3,7 @@ import Feature from '../Feature/Feature'
 
 class FeaturesList extends Component {
     render() {
-        console.log(this.props.features)
+        // console.log(this.props.features)
         const features = Object.keys(this.props.features).map((feature, idx) => {
             const featureHash = feature + '-' + idx;
             return (
