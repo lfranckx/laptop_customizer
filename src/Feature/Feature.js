e4e124eb3f01da421dc2d332e78ba7040e50879b
Bowie <bowie@thinkful.com>
2019-12-03 16:54:44 -0800

    update

--- src/Feature/Feature.js
+++ src/Feature/Feature.js
@@ -11,7 +11,8 @@ class Feature extends Component {
 
 
         const options = this.props.feature.map(item => {
-          // console.log(item)
+          console.log(item)
+          console.log(item.name)
             const itemHash = slugify(JSON.stringify(item));
             return (
               <div key={itemHash} className="feature__item">
@@ -21,7 +22,7 @@ class Feature extends Component {
                   className="feature__option"
                   name={slugify(item.name)}
                   checked={item.name === this.props.selected[this.props.name].name}
-                  onChange={e => this.props.handleUpdate(item.name, item)}
+                  onChange={e => this.props.handleUpdate(this.props.name, item)}
                 />
                 <label htmlFor={itemHash} className="feature__label">
                   {item.name} ({this.props.USPrice.format(item.cost)})
